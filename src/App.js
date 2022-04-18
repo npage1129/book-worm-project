920fe0c53c50c679b2664bdca9024ca12fea8d8d
altheajohn <98730939+altheajohn@users.noreply.github.com>
2022-04-18 12:47:08 -0600

    ,m,

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-  <Router>
-    <Routes>
-      <Route path= "/" element = {<Home/>}/>
-    </Routes>
-  </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

40cb23c807088881ac3cdd8bd77ed07d3869e101
altheajohn <98730939+altheajohn@users.noreply.github.com>
2022-04-18 11:44:48 -0600

    added logo

--- src/base/Header.js
+++ src/base/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
-function Header (){
-    return(
-        <div className = "Header">
-            
-        </div>
-    )
+function Header() {
+  return (
+    <div className="Header">
+      <H1>Header Here, Add Later</H1>
+    </div>
+  );
 }
 
 export default Header;

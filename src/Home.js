41561ca36bc35c6d80bb2d090a7b2c70542ec714
altheajohn <98730939+altheajohn@users.noreply.github.com>
2022-04-20 11:05:30 -0600

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,17 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./base/Header.js";
 import Footer from "./base/Footer.js";
 import BookList from "./BookList";
 import "./Home.css";
 
-const [book, setBook] = useState([])
-useEffect(()=> {
-  fetch(``)
-      .then((resp) => resp.json())
-      .then((data) => setBook(data.drinks))
-},[])
-
 function Home() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    fetch("https://localhost:9292/books")
+      .then((resp) => resp.json())
+      .then((data) => console.log(data.books));
+  }, []);
+
   return (
     <div className="Home">
       <Header />

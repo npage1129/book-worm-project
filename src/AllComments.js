5e6e30c70e8dc671bde1570c5aec148f53d17c73
npage1129 <82468860+npage1129@users.noreply.github.com>
2022-04-21 23:44:47 -0600

    Delete Changes Made

--- src/AllComments.js
+++ src/AllComments.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
-import CommentPage from './CommentPage'
+import CommentPage from './CommentPage';
 
-function Comments({ item, setContent, content }) {
-  const [ourComments, setOurComments] = useState([]);
+function DisplayComments({ item, setContent, content }) {
+    const [ourComments, setOurComments] = useState([]);
+    const [changingComment, setChangingComment] = useState([])
+
+
 
   function newComment(id) {
     let createComment = {
       comment: content,
       book_id: id,
     };
-
     fetch("http://localhost:9292/comments", {
       method: "POST",
       credentials: "same-origin",
@@ -24,23 +26,45 @@ function Comments({ item, setContent, content }) {
 
     setContent("");
   }
-  console.log(ourComments);
+
   let thisBooksComment = ourComments.filter(
     (comment) => comment.book_id == item.id
   );
 
+ console.log(changingComment) 
+
   const comments = ourComments;
-  useEffect(() => {
+  useEffect(() => { 
+
     fetch("http://localhost:9292/comments")
       .then((resp) => resp.json())
-      .then((data) => setOurComments(data));
+      .then((data) => setOurComments(data),
+      setChangingComment([...changingComment,thisBooksComment]));
   }, []);
 
+function handleUpdate(){
+    
+    
+}
+function handleDelete(id){
+let newSetOfComments = ourComments.filter((item) => item.id > 2)
+fetch(`http://localhost:9292/comments/${id}`,{
+      method: 'DELETE'
+  })
+  .then(res => res.json())
+  .then(data => { console.log(data) })
+  
+  fetch("http://localhost:9292/comments")
+      .then((resp) => resp.json())
+      .then((data) => setOurComments(data))
+}
   return (
     <div>
+        <h1>{item.title}</h1>
       <p>
+      
         {thisBooksComment.map((comment) => ( 
-          <>{comment.comment} </>
+          <div><p>{comment.comment}</p> <button onClick={()=>handleUpdate(comment.id)}>Update</button> <button onClick={()=>handleDelete(comment.id)}>Delete</button></div>
         ))}
       </p>
 
@@ -59,9 +83,8 @@ function Comments({ item, setContent, content }) {
         Submit
       </button>
       <div>
-      <div><CommentPage key = {item.id} content = {content} item = {item} /></div>
       </div> 
     </div>
   );
 }
-export default Comments;
+export default DisplayComments;
--- src/CommentPage.js
+++ src/CommentPage.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Comments from './Comments';
+import DisplayComments from './AllComments';
 import "./CommentPage.css";
 
 function CommentPage() {
+  const [complaints, setComplaints] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/books")
+      .then((resp) => resp.json())
+      .then((data) => setComplaints(data));
+  }, []);
+  let comments = complaints.map((comment)=> (
+  comment.comments
+ 
+  ))
+  console.log(comments)
   return (
-    <div>
-      <h1>This is the comment</h1>
-    </div>
-  );
+    <div>  
+        {complaints?.map((comment) =>  ( 
+        <DisplayComments item={comment} />
+       
+       ))} 
+   
+      </div>
+  )
+  
 }
 export default CommentPage;
